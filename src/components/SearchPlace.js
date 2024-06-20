fe327937f3e1fd7940ae0a8b35f40f6d04f5ab93
decembaek <tmdrbpp123@gmail.com>
2024-06-20 17:58:55 +0900

    지도 제작 완료, 값 리턴 구현 필요

--- src/components/SearchPlace.js
+++ src/components/SearchPlace.js
@@ -1,12 +1,20 @@
 import {
   Box,
   Button,
+  Card,
+  CardBody,
+  CardHeader,
   Center,
   HStack,
+  Heading,
   Input,
   Modal,
+  ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalOverlay,
+  Stack,
+  StackDivider,
   Text,
 } from '@chakra-ui/react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
@@ -16,11 +24,32 @@ import { useForm } from 'react-hook-form';
 
 export const SearchPlace = ({ isOpen, onClose }) => {
   const { register, handleSubmit } = useForm();
-  const [info, setInfo] = useState();
+  // const [info, setInfo] = useState();
   const [markers, setMarkers] = useState([]);
   const [map, setMap] = useState();
   const [position, setPosition] = useState();
+  // const [clickedMarkerInfo, setClickedMarkerInfo] = useState(null);
+  const [clickedLocationInfo, setClickedLocationInfo] = useState();
 
+  // 주소 - 좌표 변환 객체 생성
+  const geocoder = new window.kakao.maps.services.Geocoder();
+
+  // 검색 결과
+  const [searchResults, setSearchResults] = useState([]);
+
+  // 좌표로 주소 변환
+  // const searchLocationFromCoords = (coords, callback) => {}
+  // const searchDetailAddrFromCoords = (coords, callback) => {
+  //   geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+  // };
+
+  // 검색 기능
+  const onEnterSearch = event => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+      event.preventDefault();
+      handleSubmit(onSubmit)();
+    }
+  };
   const onSubmit = ({ search }) => {
     if (!map) return;
     const ps = new window.kakao.maps.services.Places();
@@ -42,17 +71,18 @@ export const SearchPlace = ({ isOpen, onClose }) => {
         }
         setMarkers(markers);
         map.setBounds(bounds);
+        setSearchResults(data);
       }
     });
   };
+
   const center = {
     // 지도의 중심좌표
     lat: 33.450701,
     lng: 126.570667,
   };
 
-  const [clickedLocationInfo, setClickedLocationInfo] = useState();
-
+  // 지도 화면 클릭 하면 마커 생기고 장소 나옴
   const handleMapClick = (_, mouseEvent) => {
     const latlng = mouseEvent.latLng;
     setPosition({
@@ -60,34 +90,57 @@ export const SearchPlace = ({ isOpen, onClose }) => {
       lng: latlng.getLng(),
     });
 
-    const geocoder = new window.kakao.maps.services.Geocoder();
     geocoder.coord2Address(
       latlng.getLng(),
       latlng.getLat(),
       (result, status) => {
         if (status === window.kakao.maps.services.Status.OK) {
           setClickedLocationInfo(result[0].address.address_name);
+          onSubmit({ search: result[0].address.address_name });
         }
       }
     );
+  };
+
+  // 마커 클릭 이벤트
+  const handleMarkerClick = marker => {
+    if (marker) {
+      const positionClick = marker.position;
+      setPosition({
+        lat: positionClick.lat,
+        lng: positionClick.lng,
+      });
+
+      geocoder.coord2Address(
+        positionClick.lng,
+        positionClick.lat,
+        (result, status) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            setClickedLocationInfo(result[0].address.address_name);
+            onSubmit({
+              search: result[0].address.address_name,
+            });
+            // setInfo(result[0].address.address_name);
+            // setClickedMarkerInfo(marker);
+          }
+        }
+      );
+      // console.log(marker);
+      // setInfo(marker);
+    }
   };
 
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose} size={'6xl'}>
         <ModalOverlay />
-        <ModalContent maxW={'1200px'} maxH={'1200px'}>
-          <Box
-            as="form"
-            onSubmit={handleSubmit(onSubmit)}
-            w={'100%'}
-            h={'600px'}
-            p={5}
-          >
+        <ModalContent maxW={'1200px'} maxH={'1300px'}>
+          <ModalCloseButton />
+          <Box w={'100%'} h={'800px'} p={5} mt={5}>
             {clickedLocationInfo && (
               <Box
                 position="absolute"
-                top={7}
+                top={16}
                 left={7}
                 bg="white"
                 p={4}
@@ -95,7 +148,11 @@ export const SearchPlace = ({ isOpen, onClose }) => {
                 boxShadow="md"
                 zIndex={30}
               >
-                <Text>클릭한 위치: {clickedLocationInfo}</Text>
+                <HStack>
+                  <Text>선택 위치: </Text>
+                  <Text as={'b'}>{clickedLocationInfo}</Text>
+                  <Button colorScheme="green">선택 위치로 지역 등록하기</Button>
+                </HStack>
               </Box>
             )}
             <Map
@@ -110,30 +167,89 @@ export const SearchPlace = ({ isOpen, onClose }) => {
                 <MapMarker
                   key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
                   position={marker.position}
-                  onClick={() => setInfo(marker)}
+                  onClick={event => {
+                    handleMarkerClick(marker);
+                  }}
                 >
-                  {info && info.content === marker.content && (
-                    // <div style={{ color: '#000' }}>{marker.content}</div>
-                    <Box>
-                      <Text>{marker.content}</Text>
-                    </Box>
-                  )}
+                  {/* {info && (
+                        // <div style={{ color: '#000' }}>{marker.content}</div>
+                        <>
+                          <Text>{info}</Text>
+                          <Text>{marker.content}</Text>
+                        </>
+                      )} */}
                 </MapMarker>
               ))}
               <MapMarker position={position ?? center} />
             </Map>
           </Box>
-          <Text pl={5} fontSize={'xl'}>
+
+          <Text as={'b'} pl={5} fontSize={'xl'}>
             위치 검색
           </Text>
           <Center mt={0} w={'100%'} pr={5} pl={5} pt={2} pb={5}>
             <HStack w={'100%'}>
-              <Input {...register('search')} />
-              <Button type="button" onClick={handleSubmit(onSubmit)}>
-                Search
+              <Input {...register('search')} onKeyDown={onEnterSearch} />
+              <Button
+                type="button"
+                onClick={handleSubmit(onSubmit)}
+                onKeyDown={onEnterSearch}
+                w={'40%'}
+              >
+                위치 검색
               </Button>
             </HStack>
           </Center>
+          <Card overflowY={'auto'} maxH={'500px'}>
+            <CardHeader>
+              <Heading size={'md'}>위치 목록</Heading>
+            </CardHeader>
+            <CardBody>
+              <Stack divider={<StackDivider />} spacing={4}>
+                {searchResults &&
+                  searchResults.map((result, index) => (
+                    <Box key={index}>
+                      <Box
+                        as="button"
+                        variant="outline"
+                        w="100%"
+                        textAlign="left"
+                        borderWidth="1px"
+                        borderRadius="md"
+                        p={3}
+                        _hover={{ bg: 'gray.100' }}
+                        onClick={() => {
+                          onSubmit({
+                            search: result.address_name + result.place_name,
+                          });
+                          setClickedLocationInfo(result.address_name);
+                        }}
+                      >
+                        <Heading size={'x'}>{result.address_name}</Heading>
+                        <Text pt={2} fontSize={'sm'}>
+                          {result.category_group_name}
+                        </Text>
+                        <Text pt={2} fontSize={'sm'}>
+                          {result.place_name}
+                        </Text>
+                      </Box>
+
+                      <HStack>
+                        <Button mt={2} colorScheme="green">
+                          지역 등록
+                        </Button>
+                        <Button mt={2}>네비게이션</Button>
+                      </HStack>
+                    </Box>
+                  ))}
+              </Stack>
+            </CardBody>
+          </Card>
+          <ModalFooter>
+            <Button colorScheme="red" onClick={onClose}>
+              닫기
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
